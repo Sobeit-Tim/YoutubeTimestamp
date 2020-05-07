c1195f5b21edf8c222fda3614256db7a09e31002
sukam09 <sukam09@naver.com>
2020-05-07 13:24:11 +0900

    Remove duplicated files test

--- src/Clustering.py
+++ src/Clustering.py
@@ -1,6 +1,5 @@
 import numpy as np
 from collections import Counter
-import queue
 
 file = open("stem.txt", "r")
 result = file.read()
@@ -10,145 +9,192 @@ result = result.split('\n')
 
 time = []
 stemText = []
-vocab = {}
 # 자막 파일에서 단어 수 세기.
 for i in range(0, len(result), 2):
     time.append(result[i].split(" --> "))
     temp = result[i+1].split(' ')
     stemText.append(temp)
-    for j in temp:
-        if j in vocab:
-            vocab[j] += 1
+
+#print(time)
+#print(stemText)
+partition = [0, len(stemText)]
+
+def getSortedVocab(stemTxt, start, end):
+    voCab = {}
+    for i in range(start, end):
+        for w in stemTxt[i]:
+            if w in voCab:
+                voCab[w] += 1
+            else:
+                voCab[w] = 1
+
+    #print("단어의 개수", len(voCab))
+    #print("1 개 이상의 단어의 개수", sum(x > 1 for x in voCab.values()))
+    #print(voCab)
+
+    sortedvoCab = sorted(voCab.items(), key=(lambda x: x[1]), reverse=True) # value 순 정렬.
+
+    #print(sortedvoCab)
+
+    return sortedvoCab
+
+#sortedVocab = getSortedVocab(stemText, 0, len(stemText))
+
+def printsortedVocab(sortedVocab):
+    #for printing the contents in sortedvocab
+    preValue = sortedVocab[0][1]
+    print(preValue, end=" ")
+    for (key, value) in sortedVocab:
+        if value != preValue:
+            preValue = value
+            print("\n", value, key, end=" ")
         else:
-            vocab[j] = 1
+            print(key, end=" ")
+    print("\n", end=" ")
+    return
 
-print(time)
-print(stemText)
+#printsortedVocab(sortedVocab)
 
-print("단어의 개수", len(vocab))
-print("1 개 이상의 단어의 개수", sum(x > 1 for x in vocab.values()))
-sortedVocab = sorted(vocab.items(), key=(lambda x: x[1]), reverse=True) # value 순 정렬.
-preValue = sortedVocab[0][1]
-print(" ", preValue, end=" ")
-for (key, value) in sortedVocab:
-    if value != preValue:
-        preValue = value
-        print("\n", value, key, end=" ")
-    else:
-        print(key, end=" ")
+def getfirst(word, linelist, start, end):
+    for i in range (start, end):
+        if word in linelist[i]:
+            return i
+    return -1
 
-wordVector = {}
-i = 0
-for (key, value) in sortedVocab:
-    #if len(wordVector) >= 5:
-    #    break
-    if value < 2: # 2 번 이상 쓰인 것만
-        break
-    wordVector[key] = i
-    i = i + 1
+def getlast(word, linelist, start, end):
+    for i in range (end - 1, start - 1, -1):
+        if word in linelist[i]:
+            return i
+    return -1
 
-print("dimension of vector", len(wordVector))
-print(wordVector)
+def getwordVec(sortedVocabi, StTxt, start, end, lendur):
+    WV = {}
+    i = 0
+    for (key, value) in sortedVocabi:
+        # if len(wordVector) >= 5:
+        #    break
+        if value > 1:
+            if (getlast(key, StTxt, start, end) - getfirst(key, StTxt, start, end)) > ((end - start) / lendur):
+                continue
+        if value < 2:  # 2 번 이상 쓰인 것만
+            break
+        WV[key] = i
+        i = i + 1
+    #print(WV)
+    return WV
 
-feature = np.zeros((len(stemText), len(wordVector)))
+#wordVector = getwordVec(sortedVocab, stemText, 0, len(stemText), 100)
 
-i = 0
-for line in stemText:
-    count = Counter(line)
-    for key in count:
-        if key in wordVector:
-            feature[i, wordVector[key]] = count[key]
-    i = i + 1
+#print("dimension of vector", len(wordVector))
+#print(wordVector)
 
-sumFeature = np.copy(feature)
-for i in range(len(sumFeature)):
-    if i == 0:
-        continue
-    sumFeature[i] += sumFeature[i-1]
+def getFeature(stemTxt, wordVect, start, end):
+    #print("end = ", end)
+    ft = np.zeros((end - start, len(wordVect)))
+    for i in range (end - start):
+        count = Counter(stemTxt[start + i])
+        for key in count:
+            if key in wordVect:
+                ft[i][wordVect[key]] = count[key]
+    return ft
 
+#feature = getFeature(stemText, wordVector, 0, len(stemText))
 
-norm = np.sum(feature, axis=1) # L1 norm
-print(norm)
+def getSumFeature(feat):
+    SF = np.copy(feat)
+    for i in range(len(SF)):
+        if i == 0:
+            continue
+        SF[i] += SF[i - 1]
+    return SF
+
+#sumFeature = getSumFeature(feature)
 
 
 def euc_similar(a, b):
     return np.sqrt(np.sum((a-b)**2))
 
-
-def man_similar(a, b):
-    return np.sum(np.abs(a-b))
-
+def manhattan_similar(a, b):
+    return np.sum(np.abs(a - b))
 
 def cosine_similar(a, b):  # 1 - same,  0 - perpendicular , -1 - reverse
     return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b) + 0.000001)
 
 
-def divide_cent(_feat, _s, _e, _sum): # feature의 s부터 e까지에서 반갈죽 잘하는 center를 구하자
-    if _e - _s < 3 or np.sum(_sum[_e-1] - _sum[_s]) == 0: # 구간이 일정 크기 이하 or 구간합이 0인 지점이면 그만
-        return -1
-    _index = -1
-    if _s == 0:
-        _featBase = np.zeros(np.shape(_feat[_s]))
-    else:
-        _featBase = _sum[_s - 1]
-    _min = 987654321
-    for i in range(_s, _e-1):
-        feat_a = _sum[i] - _featBase
-        if np.sum(feat_a) == 0: # 왼쪽 부분이 아직 0이면은 그냥 계속 skip
+
+def divide_cent(SF, minsize): # feature의 s부터 e까지에서 반갈죽 잘하는 center를 구하자
+    minhalf = minsize >> 1
+    maxindex = minhalf
+
+    max = 0
+    for i in range(minhalf, len(SF) - minhalf + 1):
+        feat_a = SF[i - 1]
+        if np.sum(feat_a) == 0:
             continue
         feat_a = feat_a/np.sum(feat_a)
-
-        feat_b = _sum[_e - 1] - _sum[i]
-        if np.sum(feat_b) == 0: # 오른 쪽 부분이 0이면은 break
-            break
+        feat_b = SF[-1] - SF[i - 1]
+        if np.sum(feat_b) == 0:
+            continue
         feat_b = feat_b/np.sum(feat_b)
 
-        similar = man_similar(feat_a, feat_b)
-        if similar < _min:
-            _min = similar
-            _index = i
+        #similar = euc_similar(feat_a, feat_b)
+        similar = manhattan_similar(feat_a, feat_b)
 
-    return _index
+        if similar > max:
+            max = similar
+            maxindex = i
+            #print("index i : " + str(i))
+            #print(feat_a)
+            #print(feat_b)
 
+    return maxindex
 
-# clustering. get centers.
-centers = []
-s = 0
-e = len(feature)
+def Recursion(StemTxt, start, end, minsize, iter):
+    #print(start, " " , end)
+    if end - start < minsize:
+        return
 
-cluster_queue = queue.Queue()
-cluster_queue.put((s, e))
+    sortedVocab = getSortedVocab(StemTxt, start, end)
 
-while cluster_queue.qsize() > 0:
-    s, e = cluster_queue.get()
-    if e - s < 30:
-        continue
-    centroid = divide_cent(feature, s, e, sumFeature)
-    if centroid == -1 or centroid == s or centroid == e:
-        continue
-    centers.append(centroid+1)
+    #printsortedVocab(sortedVocab)
 
-    cluster_queue.put((s, centroid+1))
-    cluster_queue.put((centroid+1, e))
+    wordVector = getwordVec(sortedVocab, StemTxt, start, end, 6) # last num is the length
 
-print(centers)
-centers = sorted(centers)
-for i in centers:
-    print("maximize similar", i, time[i], stemText[i])
+    feature = getFeature(StemTxt, wordVector, start, end)
 
+    sumFeature = getSumFeature(feature)
 
+    centroid = divide_cent(sumFeature, minsize)
+    #print (centroid)
+    #do it for front
+    Recursion(StemTxt, start, start + centroid, minsize, iter+1)
 
-"""
-0:23 : 10, The first manned, powered, heavier-than-air flight
-1:05 : 9, The Attack on Pearl Harbour
-1:45 : 8, Raising the flag on low Jima
-2:26 : 7, The Hindenburg Disaster
-3:09 : 6, V-J Day in Times Square
-3:54 : 5, Migrant Mother
-4:34 : 4, Hiroshima
-5:23 : 3, Tank Man
-6:06 : 2, The Fall of the Berlin Wall
-7:07 : 1, Man on the Moon
-"""
+    #print the result
+    #print(" " * iter, start + centroid, " ", time[start + centroid])
+    partition.append(start + centroid)
 
+    #do it for back
+    Recursion(StemTxt, start + centroid, end, minsize, iter+1)
 
+    return
+
+minsize = int(len(stemText) / 7)
+
+print("minsize = ", minsize)
+
+Recursion(stemText, 0, len(stemText), minsize ,0)
+
+partition.sort()
+print(partition)
+
+for i in range (len(partition) - 1):
+    print(time[partition[i]][0][:8],", [ " ,end = "")
+    sortvocab = getSortedVocab(stemText, partition[i], partition[i+1])
+    for j in range (4):
+        print(sortvocab[j][0].upper(), end=", ")
+    print(sortvocab[4][0].upper(), end=" ")
+    print("]")
+
+#centroid = divide_cent(sumFeature)
+
+#print("center", centroid, time[centroid])
--- src/stem.py
+++ src/stem.py
@@ -12,61 +12,63 @@ caption = yt.captions.all()[0]
 caption.download("Captions") #기본 저장 형태가 srt인듯
 """
 
-file = open("subtitle.srt", "r")
-result = file.read()
-file.close()
+def preprocessing():
+    file = open("subtitle.srt", "r")
+    result = file.read()
+    file.close()
 
-result = result.split('\n\n')
+    result = result.split('\n\n')
 
-time = []
-text = []
+    time = []
+    text = []
 
-for i in result:
-    temp = i.split('\n')
-    tempText = ""
-    for j in range(len(temp)):
-        if j == 1:
-            time.append(temp[j])
-        elif j > 1:
-            tempText += temp[j]
+    for i in result:
+        temp = i.split('\n')
+        tempText = ""
+        for j in range(len(temp)):
+            if j == 1:
+                time.append(temp[j])
+            elif j > 1:
+                tempText += temp[j]
 
-    text.append(tempText)
+        text.append(tempText)
 
-print(time)
-print(text)
+    #print(time)
+    #print(text)
 
-tokenText = []
-lemmaText = []
-stemText = []
+    tokenText = []
+    lemmaText = []
+    stemText = []
 
-# eliminateWord = ["were", "in", "a", "[", "]", "in"]
-eliminateWord = set(stopwords.words('english'))
-vocab = {}
+    # eliminateWord = ["were", "in", "a", "[", "]", "in"]
+    eliminateWord = set(stopwords.words('english'))
+    eliminateWord2 = ["'s", ".", ",", "'re", "?", "!", "'ll"]
+    vocab = {}
 
-for i in text:
-    temp = word_tokenize(i)
-    # TreebankWordTokenizer().tokenize(i)
-    # WordPunctTokenizer().tokenize(i)
-    temp = list(filter(lambda x: x not in eliminateWord, temp))
-    lemma = [WordNetLemmatizer().lemmatize(w).lower() for w in temp]
-    stem = [PorterStemmer().stem(w).lower() for w in temp]
-    if not temp:
-        continue
-    tokenText.append(temp)
-    lemmaText.append(lemma)
-    stemText.append(stem)
+    for i in text:
+        temp = word_tokenize(i)
+        # TreebankWordTokenizer().tokenize(i)
+        # WordPunctTokenizer().tokenize(i)
+        temp = list(filter(lambda x: x not in eliminateWord and x not in eliminateWord2, temp))
+        #lemma = [WordNetLemmatizer().lemmatize(w).lower() for w in temp]
+        stem = [PorterStemmer().stem(w).lower() for w in temp]
+        if not temp:
+            continue
+        #tokenText.append(temp)
+        #lemmaText.append(lemma)
+        stemText.append(stem)
 
-print(tokenText)
-print(lemmaText)
-print(stemText)
+    #print(tokenText)
+    #print(lemmaText)
+    #print(stemText)
 
-file = open("stem.txt", "w")
-for i in range(len(time)):
-    file.write(time[i] + "\n")
-    for j in range(len(stemText[i])):
-        file.write(stemText[i][j])
-        if j != len(stemText[i]) - 1:
-            file.write(" ")
-    if i != len(time) - 1:
-        file.write("\n")
-file.close()
+    file = open("stem.txt", "w")
+    for i in range(len(time)):
+        file.write(time[i] + "\n")
+        for j in range(len(stemText[i])):
+            file.write(stemText[i][j])
+            if j != len(stemText[i]) - 1:
+                file.write(" ")
+        if i != len(time) - 1:
+            file.write("\n")
+    file.close()
