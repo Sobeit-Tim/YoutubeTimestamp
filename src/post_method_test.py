3b8aed2edabb80da3e8d286eefd65cd63ed37631
sukam09 <sukam09@naver.com>
2020-05-21 03:08:31 +0900

    Remove unused files

--- src/post_method_test.py
+++
@@ -1,8 +0,0 @@
-# import requests
-
-# url = "http://localhost/InputUrl.php"
-# url = "InputUrl.php"
-# input_url = {"url": "https://www.youtube.com/watch?v=zkDMO8u9QZU"}
-# response = requests.post(url, data = input_url)
-
-# print("Input url was sent to InputUrl.php")
